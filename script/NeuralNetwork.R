6baf7d245f30e329f81c0122d971f0ec9a4e1126
lzhangUT <zhanglicau@gmail.com>
2022-05-17 11:02:56 +0800

    model evaluation and updates

--- script/NeuralNetwork.R
+++ script/NeuralNetwork.R
@@ -1,43 +1,43 @@
 library(tidyverse)
 library(neuralnet)
 
-setwd('E:/ALLFED/WheatYield_ALLFED//')
-##1. read in the cleaned file, and the selected the variables 
+setwd('E:/ALLFED/WheatYield_LOS/')
+##1.1 read in the cleaned file, and the selected the variables 
 df = read.csv('data/dwheat_fit_elim.csv')
-df_rf = df %>% dplyr::select(Y, n_total,  irrigation_tot,
+df_nn = df %>% dplyr::select(Y, n_total,  irrigation_tot,
                              mechanized, pesticides_H, thz_class,
                              mst_class, soil_class)
 
 
-# Scale the Data
+##1.2 Scale the Data
 scale01 = function(x){(x - min(x)) / (max(x) - min(x))}
-df_rf = df_rf %>% mutate_all(scale01)
+train_nn = df_nn %>% mutate_all(scale01)
 
-# Build Neural Network
-n = names(df_rf)
+##2.1 Build Neural Network
+n = names(train_nn)
 f = as.formula(paste("Y ~", paste(n[!n %in% "Y"], collapse = " + ")))
 print(f)
-nn = neuralnet(f,data=df_rf,hidden=c(3),linear.output=T)
+nn = neuralnet(f,data=train_nn,hidden=c(3),linear.output=T)
 saveRDS(nn,file='output/NeuralNetworkResults.rds')
-# Predict on data
-pr.nn <- compute(nn,df_rf[,-1])
 
-# Compute mean squared error
-pr.nn_ <- pr.nn$net.result * (max(df_rf$Y) - min(df_rf$Y)) + min(df_rf$Y)
-test.r <- (df_rf$Y) * (max(df_rf$Y) - min(df_rf$Y)) + min(df_rf$Y)
-MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(df_rf)
-print(MSE.nn)
+##2.2 model performance on the training
+train.nn = compute(nn,train_nn[,-1])
 
-# Plot the neural network
+##2.3 Compute mean squared error on the train dataset
+pred_train = train.nn$net.result * (max(train_nn$Y) - min(train_nn$Y)) + min(train_nn$Y)
+obs_train = (train_nn$Y) * (max(train_nn$Y) - min(train_nn$Y)) + min(train_nn$Y)
+MSE_train = sum((obs_train - pred_train)^2) / nrow(train_nn)
+print(MSE_train)
+
+##2.4 Plot the neural network
 plot(nn)
 
-# Plot regression line/Check the model performance
-reg = lm(df_rf$Y~pr.nn_)
-reg_sum = summary(reg)
-r2 = reg_sum$adj.r.squared
+##2.5 Plot regression line/Check the model performance---training dataset
+reg_train = lm(train_nn$Y~pred_train)
+r2 = summary(reg_train)$adj.r.squared
 
-png(filename="output/Figure3-ModelPerformanceNN.png")
-plot(df_rf$Y, pr.nn_, xlab='Observed',ylab='Predicted',
+png(filename="output/Figure1a-ModelPerformanceNN_train.png")
+plot(train_nn$Y, pred_train, xlab='Wheat Production_train (Kg/ha)',ylab='Wheat Prediction (Kg/ha)',
      xlim = c(0,1), ylim=c(0,1))
 abline(0, 1, lwd = 2)
 
@@ -45,17 +45,52 @@ mylabel = bquote(italic(R)^2 == .(format(r2, digits = 3)))
 text(x = 0.5, y = 0.8, labels = mylabel)
 dev.off()
 
-#####variable importance from NN
+##3. variable importance from NN
 require(devtools)
 #import 'gar.fun' from Github
 source_gist('6206737')
 
-#create a pretty color vector for the bar plot
-cols<-colorRampPalette(c('lightgreen','lightblue'))(7)
 #use the function on the model created above
 par(mar=c(3,4,1,1),family='serif')
 plotdata = gar.fun('Y',nn)
 
 plotdata + ylim(-1,1)+ylab('Relative Importance')
-ggsave(('output/Figure4-VariableImportanceNN.png'))
+ggsave(('output/Figure2-VariableImportanceNN.png'))
+
+####4. model performance on the test data
+###4.1 read in the rest data and make prediciton using NN
+test_y = read.csv('data/dwheat_val_elim.csv')
+test_y = test_y %>% select(Y)
+test_x = read.csv('data/w_val_elim.csv')
+test_x = test_x %>% select(n_total, irrigation_tot,mechanized,
+                            pesticides_H, thz_class, mst_class, soil_class)
+
+test_nn = test_y %>% bind_cols(test_x) %>% mutate_all(scale01)
+
+test.nn = compute(nn,test_nn[,-1])
+
+##4.2 Compute mean squared error on the test dataset
+pred_test = test.nn$net.result * (max(test_nn$Y) - min(test_nn$Y)) + min(test_nn$Y)
+obs_test = (test_nn$Y) * (max(test_nn$Y) - min(test_nn$Y)) + min(test_nn$Y)
+MSE_test= sum((obs_test - pred_test)^2) / nrow(test_nn)
+print(MSE_test)
+
+
+##4.3 Plot regression line/Check the model performance---test dataset
+reg_test = lm(test_nn$Y~pred_test)
+r2 = summary(reg_test)$adj.r.squared
+
+png(filename="output/Figure1b-ModelPerformanceNN_test.png")
+plot(test_nn$Y, pred_test, xlab='Wheat Production_test (Kg/ha)',ylab='Wheat Prediction (Kg/ha)',
+     xlim = c(0,1), ylim=c(0,1))
+abline(0, 1, lwd = 2)
+
+mylabel = bquote(italic(R)^2 == .(format(r2, digits = 3)))
+text(x = 0.5, y = 0.8, labels = mylabel)
+dev.off()
+
+
+
+
+
 
--- script/RandomForest.R
+++ script/RandomForest.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(randomForest)
 library(psych)
 
-setwd('E:/ALLFED/WheatYield_ALLFED//')
+setwd('E:/ALLFED/WheatYield_LOS/')
 ##1. read in the cleaned file, and select the variables under consideration
 df = read.csv('data/dwheat_fit_elim.csv')
 df_rf = df %>% dplyr::select(Y, n_total, p_fertilizer, irrigation_tot,
@@ -17,18 +17,18 @@ str(df_rf)
 corPlot(df_rf[,-1], cex = 1.2) ##n_total is highly correlated with p_ferilizer
 
 ###remove p_fertilizer and NAs 
-df_rf = df_rf %>% dplyr::select(-p_fertilizer) %>% filter(complete.cases(.))
-dim(df_rf)
+train_rf = df_rf %>% dplyr::select(-p_fertilizer) %>% filter(complete.cases(.))
+dim(train_rf)
 ##3. random forest on whole training set 'df_rf'
 set.seed(1234)
 
 ###3.1 run RF with ntree=500, ntree=1000 didnt work as run out of memory
-rf_mod = randomForest(Y~.,data=df_rf,importance=TRUE, ntree=500)
+rf_mod = randomForest(Y~.,data=train_rf,importance=TRUE, ntree=500)
 #saveRDS(rf_mod,file='output/RandomForestResults.rds')
 print(rf_mod)
 
 ###3.2 Find the optimal mtry value
-mtry <- tuneRF(df_rf[-1],df_rf$Y, ntreeTry=500,
+mtry <- tuneRF(train_rf[-1],train_rf$Y, ntreeTry=500,
                stepFactor=1.5,improve=0.01, trace=TRUE, plot=TRUE)
 print(mtry)
 
@@ -36,35 +36,65 @@ best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
 print(best.m)
 
 ###3.3 build model with again using best mtry value
-best.m=3
-rf_mod2 = randomForest(Y~.,data=df_rf,importance=TRUE, ntree=500, mtry= best.m)
+best.m=3 ###from previous step
+rf_mod2 = randomForest(Y~.,data=train_rf,importance=TRUE, ntree=500, mtry= best.m)
 saveRDS(rf_mod2,file='output/RandomForestResults.rds')
 print(rf_mod2)
 
 importance(rf_mod2)
 varImpPlot(rf_mod2)
 
-##get the prediction from the model
-pred_Y= predict(rf_mod2,df_rf)
-obs_pre = cbind(data.frame(Y=df_rf$Y),data.frame(pred_Y=pred_Y))
-obs_pre = as.data.frame(obs_pre)
+##3.4 check the model performance for the training dataset---model calibration
+pred_train= predict(rf_mod2,train_rf)
+train_both = cbind(data.frame(obs_train=train_rf$Y),data.frame(pred_train=pred_train))
+train_both = as.data.frame(train_both)
 
-###get correlation (R2) between the prediction and the observations
-reg =lm(pred_Y~Y,data=obs_pre)
-summary(reg)
-eq_lm =paste0(expression(R^2),'==', round(summary(reg)$r.squared,2))
-eq_lm = data.frame(eq_lm)
-eq_lm$eq_lm = as.character(eq_lm$eq_lm)
+###3.5 get correlation (R2) between the prediction and the observations
+reg_train =lm(pred_train~obs_train,data=train_both)
+summary(reg_train)
+lm_train =paste0(expression(R^2),'==', round(summary(reg_train)$r.squared,2))
+lm_train = data.frame(lm_train)
+lm_train$lm_train = as.character(lm_train$lm_train)
 
-ggplot(obs_pre,aes(Y,pred_Y))+geom_point()+
-geom_abline()+theme_bw()+xlab('Wheat Production (Kg/ha)')+
+ggplot(train_both,aes(obs_train,pred_train))+geom_point()+
+geom_abline()+theme_bw()+xlab('Wheat Production_train (Kg/ha)')+
   ylab('Wheat Prediciton (Kg/ha)')+
   theme(axis.text=element_text(size=12,face = 'bold'),
         text = element_text(size=15,face = 'bold'))+
-  geom_text(data = eq_lm, aes(x = 3000, y = 9000, label = eq_lm, family = "serif"), 
+  geom_text(data = lm_train, aes(x = 3000, y = 9000, label = lm_train, family = "serif"), 
             size=6, parse = TRUE) 
 
-ggsave('output/Figure1-ModelPerformance.png')
+ggsave('output/Figure1a-ModelPerformance_train.png')
+
+###3.6 check the model performance with the evaluation dataset---model evaluation
+test_y = read.csv('data/dwheat_val_elim.csv')
+test_y = test_y %>% select(Y)
+test_x = read.csv('data/w_val_elim.csv')
+test_rf = test_x %>% select(n_total, irrigation_tot,mechanized,
+                          pesticides_H, thz_class, mst_class, soil_class)
+
+pred_test= predict(rf_mod2,test_rf)
+test_both= cbind(data.frame(obs_test=test_y$Y),data.frame(pred_test=pred_test))
+test_both = as.data.frame(test_both)
+
+###3.7 visualization of model evaluation
+reg_test =lm(pred_test~obs_test,data=test_both)
+summary(reg_test)
+lm_test =paste0(expression(R^2),'==', round(summary(reg_test)$r.squared,2))
+lm_test = data.frame(lm_test)
+lm_test$lm_test= as.character(lm_test$lm_test)
+
+ggplot(test_both,aes(obs_test,pred_test))+geom_point()+
+  geom_abline()+theme_bw()+xlab('Wheat Production_test (Kg/ha)')+
+  ylab('Wheat Prediciton (Kg/ha)')+
+  theme(axis.text=element_text(size=12,face = 'bold'),
+        text = element_text(size=15,face = 'bold'))+
+  geom_text(data = lm_test, aes(x = 3000, y = 9000, label = lm_test, family = "serif"), 
+            size=6, parse = TRUE) 
+
+ggsave('output/Figure1b-ModelPerformance_test.png')
+
+
 
 ##check the variable importance 
 varImpPlot(rf_mod2,main="Variable Importance")
@@ -82,28 +112,29 @@ ggplot(VIPVar, aes(x=reorder(Variables, X.IncMSE), weight=X.IncMSE)) +geom_bar()
 
 ggsave('output/Figure2-VariableImportance_IncMSE.png')
 
-##########predicting if loss of industry (los)
+##########predicting if loss of industry (los) with training dataset
 
-print(names(df_rf))
+print(names(train_rf))
 
-pred_Y_los= vector('list', 0)
-for (i in 2:length(names(df_rf))){
+pred_los= vector('list', 0)
+for (i in 2:length(names(train_rf))){
   ###change each column into zero but name them the same 
-  df_pre = df_rf %>% select(-i) %>% mutate('los'=0)
-  colnames(df_pre)[ncol(df_pre)] = names(df_rf)[i]
+  train_pre = train_rf %>% select(-all_of(i)) %>% mutate('los'=0)
+  colnames(train_pre)[ncol(train_pre)] = names(train_rf)[i]
+  train_pre = train_pre %>% select(names(train_rf)[-1])
   
   ###predict with los using model 
-  tmp = as.data.frame( predict(rf_mod2,df_pre))
-  colnames(tmp) = paste0(names(df_rf)[i],'_los')
-  pred_Y_los[[i-1]] = tmp
+  tmp = as.data.frame( predict(rf_mod2,train_pre))
+  colnames(tmp) = paste0(names(train_rf)[i],'_los')
+  pred_los[[i-1]] = tmp
 }
 
-out =bind_cols(pred_Y_los)
-out_all = cbind(df_rf$Y, out)
+out =bind_cols(pred_los)
+out_all = cbind(train_rf$Y, out)
 colnames(out_all)[1]='Y'
 
 
-png(filename="output/Figure5-Boxplots_los.png", width = 1000)
+png(filename="output/Figure3-Boxplots_train_los.png", width = 1000)
 boxplot(out_all)
 dev.off()
 
